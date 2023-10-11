3af08a25201cea07642564cb88f716af11346581
Eilzaveta <eilzaveta@MacBook-Air-Eilzaveta.local>
2023-10-11 18:46:12 +0300

    Добавлено решение

--- src/lab1/calculator.py
+++ src/lab1/calculator.py
@@ -1,29 +1,25 @@
-a = "0"
-print("Введите Enter что бы закончить")
-while a:
-    a = input("Введите первое число: ")
-    if a == "":
+"""Calculator"""
+print("Калькулятор работает только с целыми числами и операциями -+;/")
+print("Пример ввода:\n1-8*17+13\nВывод: -122")
+s = input().replace("+", " + ").replace("-", " - ").replace("/", " / ").replace("*", " * ").split()
+d = [s[0]]
+for i in s[1:]:
+    if i in "+-*/":
+        d.append(i)
+    elif i.isdigit() and d[-1] in "+-":
+        d.append(i)
+    elif i.isdigit() and d[-1] in "*" and d[-2] not in "-+*/":
+        del d[-1]
+        d[-1] = str(int(d[-1]) * int(i))
+    elif i.isdigit() and d[-1] in "/" and d[-2] not in "-+*/":
+        if int(i) == 0:
+            print("ERROR")
+            break
+        del d[-1]
+        d[-1] = str(int(d[-1]) / int(i))
+    else:
+        print("ERROR")
         break
-    while not a.replace(".", "").isdigit() or a.count(".") > 1:
-        print("Это не число!!")
-        a = input("Введите первое число: ")
-    b = input("Введите второе число: ")
-    while not b.replace(".", "").isdigit() or b.count(".") > 1:
-        print("Это не число!!")
-        b = input("Введите второе число: ")
-    s = input("Введите операцию(+-*/): ")
-    while s not in ["+", "-", "*", "/"]:
-        print("Операция некорректна!!")
-        b = input("Введите корректную операцию(+-*/): ")
-    a1, b1 = float(a), float(b)
-
-    if b1 == 0 and s == "/":
-        print("Деление на 0 невозможно")
-    elif s == "-":
-        print(a1 - b1)
-    elif s == "+":
-        print(a1 + b1)
-    elif s == "*":
-        print(a1 * b1)
-    elif s == "/":
-        print(a1 / b1)
+else:
+    d = "".join(d).replace("-", " -").replace("+", " ").split()
+    print(sum(map(float, d)))
