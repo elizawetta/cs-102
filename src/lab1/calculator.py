f7733dbd6fbec884ca198716b10bf95d8c957ad7
Eilzaveta <eilzaveta@MacBook-Air-Eilzaveta.local>
2023-10-11 21:07:04 +0300

    Добавлено решение

--- src/lab1/calculator.py
+++ src/lab1/calculator.py
@@ -1,20 +1,37 @@
 """Calculator"""
+
+
+def is_number(num):
+    """check string is number"""
+    return num.replace("-", "").replace(".", "").isdigit()
+
+
 print("Калькулятор работает только с целыми числами и операциями -+*/")
 print("Пример ввода:\n1-8*17+13\nВывод: -122")
 
-s = input().replace("+", " + ").replace("-", " - ").replace("/", " / ").replace("*", " * ").split()
-d = [s[0]]
-if not all(map(lambda x: (x in "-+*/" or x.isdigit()), s)) or s[-1] in "*-+/" or len(s) == 0:
-    s, d = [], []
-for i in s[1:]:
+S = input().replace(" ", "").replace("--", "+").replace("++", "+")
+if S.count("/+") + S.count("*+") + S.count("*/") + S.count("/*") + S.count("+/") + S.count("+*"):
+    S = "-"
+S = S.replace("+", " + ").replace("-", " - ").replace("/", " / ").replace("*", " * ")
+S = S.replace("/  - ", "/ -").replace("*  - ", "* -").replace("+  - ", "+ -")
+if S[0:2] in [" -", " +"]:
+    S = "0" + S
+
+S = S.split()
+d = [S[0]]
+if not all(map(lambda x: (x in "-+*/" or is_number(x)), S)):
+    S, d = [], []
+elif S[-1] in "*-+/" or S[0] in "-+" or len(S) == 0:
+    S, d = [], []
+for i in S[1:]:
     if i in "+-*/":
         d.append(i)
-    elif i.isdigit() and d[-1] in "+-":
+    elif is_number(i) and d[-1] in "+-":
         d.append(i)
-    elif i.isdigit() and d[-1] in "*" and d[-2].replace(".", "").isdigit():
+    elif is_number(i) and d[-1] in "*" and is_number(d[-2]):
         del d[-1]
         d[-1] = str(float(d[-1]) * int(i))
-    elif i.isdigit() and d[-1] in "/" and d[-2].replace(".", "").isdigit():
+    elif is_number(i) and d[-1] in "/" and is_number(d[-2]):
         if int(i) == 0:
             print("ERROR")
             break
@@ -27,5 +44,11 @@ else:
     if len(d) == 0:
         print("ERROR")
     else:
-        d = "".join(d).replace("-", " -").replace("+", " ").split()
+        d = ("".join(d)
+            .replace("-+", "-")
+            .replace("--", "+")
+            .replace("-", " -")
+            .replace("+", " ")
+            .split()
+        )
         print(sum(map(float, d)))
