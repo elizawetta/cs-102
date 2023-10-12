e8209a5d8c0c8e906f0bb28d3becc8cd2bf8144c
Eilzaveta <eilzaveta@MacBook-Air-Eilzaveta.local>
2023-10-12 19:57:18 +0300

    реализованны функции encrypt_vigenere и decrypt_vigenere

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -9,7 +9,14 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.lower()
+    for i in range(len(plaintext)):
+        shift = ord(keyword[i % len(keyword)]) - ord('a')
+        if plaintext[i].isupper():
+            ciphertext += chr(ord('a') + (ord(plaintext[i].lower()) - ord('a') + shift) % 26).upper()
+        else:
+            ciphertext += chr(ord('a') + (ord(plaintext[i].lower()) - ord('a') + shift) % 26)
+
     return ciphertext
 
 
@@ -24,5 +31,12 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.lower()
+    for i in range(len(ciphertext)):
+        shift = ord(keyword[i % len(keyword)]) - ord('a')
+        if ciphertext[i].isupper():
+            plaintext += chr(ord('a') + (ord(ciphertext[i].lower()) - ord('a') - shift) % 26).upper()
+        else:
+            plaintext += chr(ord('a') + (ord(ciphertext[i].lower()) - ord('a') - shift) % 26)
     return plaintext
+
